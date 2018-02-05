9091c5a49d43af7fcedcc903deb0e7b9ab05fe8f
skreets <mail@skreets.ru>
2018-02-05 03:24:26 +0300

    config .ck, layout

---
+++ source/static/jquery/main.js
@@ -0,0 +1,7 @@
+// @codekit-prepend "../../../bower_components/jquery/dist/jquery.min.js";
+// @codekit-prepend "../../../bower_components/tooltipster/dist/js/tooltipster.bundle.min.js";
+// @codekit-prepend "../../../bower_components/slick-carousel/slick/slick.min.js";
+
+$(document).ready(function() {
+  
+});
