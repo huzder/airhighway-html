4512598dfa1dbb9d5be7d3f04be3972ff960265b
skreets <mail@skreets.ru>
2018-02-13 14:30:48 +0300

    fix import js & scss

--- source/static/jquery/script.js
+++ source/static/jquery/script.js
@@ -1,8 +1,3 @@
-// @codekit-prepend "../../../bower_components/jquery/dist/jquery.min.js";
-// @codekit-prepend "../../../bower_components/tooltipster/dist/js/tooltipster.bundle.min.js";
-// @codekit-prepend "../../../bower_components/magnific-popup/dist/jquery.magnific-popup.min.js";
-// @codekit-prepend "../../../bower_components/slick-carousel/slick/slick.min.js";
-
 $(document).ready(function() {
 
   $('body').on('click', '.burger', function(){
