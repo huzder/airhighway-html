b040ace90cc377464951745b73c89bc43f365bb4
skreets <skreets.ru@yandex.ru>
2018-07-13 13:35:14 +0500

    fixed menu

--- source/static/jquery/script.js
+++ source/static/jquery/script.js
@@ -1,11 +1,13 @@
 $(document).ready(function() {
 
-  $('body').on('click', '.burger', function(){
+  $('header').scrollToFixed();
+
+  $('html.mobile body, html.tablet body').on('click', '.burger', function(){
     $(this).toggleClass('active');
     $('header div nav, header div div').toggleClass('active');
   });
 
-  $('body').on('click', 'header nav ul li span', function(){
+  $('html.mobile body, html.tablet body').on('click', 'header nav ul li span', function(){
     $(this).next('ul').toggleClass('active');
   });
 
